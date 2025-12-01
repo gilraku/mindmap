4188e3f7bcff7ac287fde3bbb84b996cbde0604e
Gilang <gilangswandaru27@gmail.com>
2025-12-01 22:18:20 +0800

    push kedua

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    base: './',      // penting untuk deploy statis
     server: {
         watch: {
             usePolling: true,
